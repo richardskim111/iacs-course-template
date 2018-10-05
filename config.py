bded49d0c0e5612f46f3560abb79d2cc41404b73
Richard Kim <kimr@mit.edu>
2018-10-05 14:06:23 -0400

    modify config.py

--- config.py
+++ config.py
@@ -38,11 +38,21 @@ CATEGORIES_SAVE_AS = ''
 
 ARCHIVES_SAVE_AS = ''
 
-TAGS_SAVE_AS = ''
+# TAGS_SAVE_AS = 'topics/index.html'
+#
+# TAG_SAVE_AS = 'topics/{slug}/index.html'
+#
+# TAG_URL = 'topics/{slug}/'
 
-ARTICLE_SAVE_AS = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+
+ARTICLE_URL = '{category}/{slug}/'
 
 AUTHOR_URL = ''
+
+AUTHOR_SAVE_AS = ''
+
+INDEX_SAVE_AS = 'pages/materials.html'
 
 THEME_STATIC_DIR = 'style'
 
@@ -50,13 +60,23 @@ DELETE_OUTPUT_DIRECTORY = True
 
 MARKUP = ['md', 'ipynb']
 
-PLUGIN_PATHS = ['./plugins']
+PLUGIN_PATHS = ['plugins']
 
-PLUGINS = ['ipynb.markup', 'dateish']
+PLUGINS = ['ipynb.markup', 'tipue_search']
 
 IGNORE_FILES = ['.ipynb_checkpoints']
 
-DATEISH_PROPERTIES = ['due']
-
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+STATIC_PATHS = ['lectures', 'labs', 'homeworks', 'sections', 'wiki', 'images', 'projects']
+
+DIRECT_TEMPLATES = ['index', 'category', 'tags', 'search']
+
+import re
+
+JINJA_FILTERS = {
+    'original_content': lambda x: re.search(r"content/.*", x).group(0)
+}
+
+USE_FOLDER_AS_CATEGORY = False
